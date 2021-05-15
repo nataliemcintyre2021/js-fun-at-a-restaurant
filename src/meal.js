c34273c0239533490bd9f0fd4c80a94e17a4c8e5
Natalie McIntyre <nataliemcintyre2021@gmail.com>
2021-05-14 18:44:40 -0700

    Pass meal tests

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,50 @@
-function nameMenuItem() {
-
+function nameMenuItem(menuItemName) {
+  return `Delicious ${menuItemName}`;
 }
 
+function createMenuItem(name, price, type) {
+  return (
+    menuItem = {
+      name: name,
+      price: price,
+      type: type
+    });
+}
+
+function addIngredients(newIngredient, ingredients) {
+  if (ingredients.includes(newIngredient)) {
+    return
+  } else {
+   return ingredients.push(newIngredient);
+ }
+};
+
+function formatPrice(initialPrice) {
+  return `$${initialPrice}`
+};
+
+function decreasePrice(price) {
+  var discount = (price * .10);
+  var decreasedPrice = (price - discount);
+  return decreasedPrice
+};
+
+
+function createRecipe(title, ingredients, menuItemType) {
+  return (
+    recipe = {
+      title: title,
+      ingredients: ingredients,
+      type: menuItemType
+    }
+  )
+};
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
